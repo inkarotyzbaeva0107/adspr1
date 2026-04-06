818726921fd8f18f7ab10a7a717d789b509280d5
inkar <inkarotyzbaeva0107@gmail.com>
2026-04-06 14:04:08 +0500

    1 task

---
+++ src/FibonacciCallsCount.java
@@ -0,0 +1,24 @@
+public class FibonacciCallsCount {
+
+    static int callCount = 0;
+
+    public static int fib(int n) {
+        callCount++;
+
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+
+        callCount = 0;
+
+        int result = fib(n);
+
+        System.out.println("fib(" + n + ") = " + result);
+        System.out.println("Number of calls: " + callCount);
+    }
+}
